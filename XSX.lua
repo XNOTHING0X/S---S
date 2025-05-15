6eff0466d1e4b38eee05990c523cdd2eaf55997d
XNOTHING0X <nothingx9666@gmail.com>
2025-05-15 16:43:13 +0200

    Update XSX.lua

--- XSX.lua
+++ XSX.lua
@@ -1,25 +1,26 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
-local hasSentFlag = ReplicatedStorage:FindFirstChild("HasSentNotification")
+local hasSentFlag = ReplicatedStorage:FindFirstChild("NOTHING X HUB")
 if not hasSentFlag then
     hasSentFlag = Instance.new("BoolValue")
-    hasSentFlag.Name = "HasSentNotification"
+    hasSentFlag.Name = "NOTHING X HUB"
     hasSentFlag.Value = false
     hasSentFlag.Parent = ReplicatedStorage
 end
 if not hasSentFlag.Value then
-    game:GetService("StarterGui"):SetCore("SendNotification", {
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
         Title = "",
         Text = "",
         Duration = 5.5,
     })
-    game:GetService("StarterGui"):SetCore("SendNotification", {
+    StarterGui:SetCore("SendNotification", {
         Title = "NOTHING X HUB",
         Text = "",
         Duration = 5.5,
     })
-    game:GetService("StarterGui"):SetCore("SendNotification", {
+    StarterGui:SetCore("SendNotification", {
         Title = "",
         Text = "",
         Duration = 5.5,

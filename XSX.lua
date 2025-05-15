9a47d54e624534119014abb4d4207c5e6eb93886
XNOTHING0X <nothingx9666@gmail.com>
2025-05-15 16:30:43 +0200

    Update XSX.lua

--- XSX.lua
+++ XSX.lua
@@ -3,10 +3,10 @@ local player = game:GetService("Players").LocalPlayer
 local hasSent = false
 
 if not hasSent then
+        hasSent = true
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "",
         Text = "",
         Duration = 5.5,
     })
-    hasSent = true
 end

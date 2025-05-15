6487dbab266878261171c127833ec1a472d17aac
XNOTHING0X <nothingx9666@gmail.com>
2025-05-15 16:35:53 +0200

    Update XSX.lua

--- XSX.lua
+++ XSX.lua
@@ -1,18 +1,12 @@
 local player = game:GetService("Players").LocalPlayer
-
--- Używamy ReplicatedStorage do przechowywania flagi globalnie
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local hasSentFlag = ReplicatedStorage:FindFirstChild("HasSentNotification")
-
--- Tworzymy flagę, jeśli nie istnieje
 if not hasSentFlag then
     hasSentFlag = Instance.new("BoolValue")
     hasSentFlag.Name = "HasSentNotification"
     hasSentFlag.Value = false
     hasSentFlag.Parent = ReplicatedStorage
 end
-
--- Wysyłamy powiadomienie tylko raz
 if not hasSentFlag.Value then
     game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "",
